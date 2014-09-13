9eec12044666dfb11542459a562bb7e2083225fb
Aaron Chung <a_h_chung@yahoo.com>
2014-09-12 22:15:45 -0700

    login, logout, some error messages (a la flash data) have been implemented

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var express = require('express.io');
-// var morgan = require('morgan');
 var path = require('path');
 var app = express().http().io();
 var http = require('http');
@@ -11,39 +10,27 @@ app.configure(function() {
     app.use(express.static(path.join(__dirname, 'public')));
     app.use(express.session({secret: 'monkey'}));
     app.set('view engine', 'ejs');
-
     app.set('port', process.env.PORT || 1111);
     app.set('views', path.join(__dirname, 'views'));
-<<<<<<< HEAD
-    // app.use(express.logger('dev'));      // this line is preventing
-                                            // socket.io.js from being served
-    // app.use(morgan('dev'));                 // morgan is the updated logger...
-=======
     app.use(express.logger('dev'));
->>>>>>> parent of 433ec62... going to try to install morgan for logger use...  with express.io using express 3...
     app.use(express.methodOverride());
     app.use(app.router);
 });
 
+var mongoose = require('./config/mongoose');
+var routes = require('./config/routes')(app);
 
-
-// ---- from mongoose section's app.js ----
-// app.set('port', process.env.PORT || 1111);
-// app.set('views', path.join(__dirname, 'views'));
-// app.use(express.logger('dev'));
-// app.use(express.methodOverride());
-// app.use(app.router);
 // development only
 if ('development' == app.get('env')) {
     app.use(express.errorHandler());
 }
-var mongoose = require('./config/mongoose');
-var routes = require('./config/routes')(app);
-http.createServer(app).listen(app.get('port'), function() {
-    console.log('Express server listening on port ' + app.get('port'));
-});
+// http.createServer(app).listen(app.get('port'), function() {
+//     console.log('Express server listening on port ' + app.get('port'));
+// });
 // ----------------------------------------
 
+app.listen(1111);
+console.log('Express server listening on port ' + app.get('port'));
 
 app.io.route('order_ice_cream', function(req) {
     console.log('received an event', req.data);
--- config/routes.js
+++ config/routes.js
@@ -7,13 +7,29 @@ module.exports = function Route(app) {
   		res.render('welcome', { title: 'Welcome' });
   	})
   	app.get('/welcome', function(req, res) {
-  		res.render('welcome', { title: 'Welcome'});
+  		if (typeof req.session.notice !== "undefined") {
+  			var notice = req.session.notice;
+  			delete req.session.notice;
+  		}
+  		else if (typeof req.session.error !== "undefined") {
+  			var error = req.session.error;
+  			delete req.session.error;
+  		};
+  		res.render('welcome', { title: 'Welcome', notice: notice, error: error });
 	});
 	app.get('/register', function(req, res) {
 		res.render('register', { title: 'Registration'});
 	});
 	app.get('/main', function(req, res) {
-		res.render('main', { title: 'Main'});
+		if (typeof req.session.logged_user_id !== "undefined") {
+			res.render('main', { title: 'Main', session_info: req.session });
+		}
+		else {
+			req.session.notice = "Please log in.";
+			req.session.save(function() {
+				res.redirect('/welcome');
+			})
+		}
 	});
 	app.post('/users/create', function(req, res) {
 		// post data usually in req.body; here, post data was submitted inside
@@ -22,6 +38,44 @@ module.exports = function Route(app) {
 		a.save(function(err, a) {
 			console.log(err, a);
 			return res.redirect('/welcome');
+		});
+	});
+	app.post('/sessions/create', function(req, res) {
+		var mail = req.body.user.email;
+		var pwd = req.body.user.password;
+		// console.log("searching for: " + email + " @ " + pwd);
+		User.find({email: mail, password: pwd}, function(err, results) {
+			if (results.length == 0) {
+				req.session.error = "There is no user registered with that email and password.";
+				req.session.save(function() {
+					res.redirect('/welcome');
+				})
+			}
+			else {
+				// the results are returned in an array of objects
+				var data = results[0];
+				req.session.first_name = data.first_name;
+				req.session.last_name = data.last_name;
+				req.session.logged_user_id = data._id;
+				req.session.sessionID = req.sessionID;
+				req.session.save(function() {
+					res.redirect('/main');
+				})
+				// res.send(results);
+			};
+		});
+	});
+	app.get('/signout', function(req, res) {
+		req.session.destroy(function() {
+			res.redirect('/welcome');
+		});
+	});
+
+
+
+	app.get('/test', function(req, res) {		// for debugging purposes... i can
+		User.find({}, function(err, results) {  // view all users in the database
+			res.send(results);					// with this api call
 		});
 	});
 };
